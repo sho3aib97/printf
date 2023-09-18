732f80d312f8334a9fb425bd371663c627cd3390
hendzaineddin <hendzad.32@gmail.com>
2023-09-18 11:05:57 -0700

    l

---
+++ _num.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * numc - function counts the numbers of digits.
+ * @n: the digits.
+ * Return: count.
+ */
+
+int numc(int n)
+{
+	int count = 0;
+
+	if (n < 0)
+	count += 1;
+
+	do {
+	n /= 10;
+	++count;
+	} while (n != 0);
+
+
+	return (count);
+}
